41afa5301c8b6f7432a5bc9d6dc9e22177096d04
Shivendra Verma <Shivendrasony@gmail.com>
2018-07-04 18:39:48 +0530

    new lay out

--- src/components/Calc/Calc.js
+++ src/components/Calc/Calc.js
@@ -31,31 +31,34 @@ class Calc extends React.Component {
       }
     render() {
         return(
-            <div className="wrapper">
-        <div  className="calc">
-          <textarea className="textarea" id="display" value={this.state.expression} readOnly ></textarea>
-          <div className="buttons"> 
-            <button value="1" className="btn" onClick={this.handleClick}>1</button>
-            <button value="2" className="btn" onClick={this.handleClick}>2</button>
-            <button value="3" className="btn" onClick={this.handleClick}>3</button>
-            <button value="+" className="btn" onClick={this.handleClick}>+</button>
-            <button value="4" className="btn" onClick={this.handleClick}>4</button>
-            <button value="5" className="btn" onClick={this.handleClick}>5</button>
-            <button value="6" className="btn" onClick={this.handleClick}>6</button>
-            <button value="-" className="btn" onClick={this.handleClick}>-</button>
-            <button value="7" className="btn" onClick={this.handleClick}>7</button>
-            <button value="8" className="btn" onClick={this.handleClick}>8</button>
-            <button value="9" className="btn" onClick={this.handleClick}>9</button>
-            <button value="*" className="btn" onClick={this.handleClick}>*</button>
-            <button value="0" className="btn" onClick={this.handleClick}>0</button>
-            <button value="." className="btn" onClick={this.handleClick}>.</button>
-            <button className="btn" onClick={this.clear}>AC</button>
-            <button value="/" className="btn" onClick={this.handleClick}>/</button>
-            <button id="back" onClick={this.back}>back</button>       
-            <button id="AC" onClick={this.evaluate}>=</button>
-          </div>
+        <div className="wrapper">
+            <header className="header">
+                <h3>CALCULATOR</h3>
+            </header>
+            <div  className="calc">
+                <textarea className="textarea" id="display" value={this.state.expression} readOnly ></textarea>
+                <div className="buttons"> 
+                    <button value="1" className="btn" onClick={this.handleClick}>1</button>
+                    <button value="2" className="btn" onClick={this.handleClick}>2</button>
+                    <button value="3" className="btn" onClick={this.handleClick}>3</button>
+                    <button value="+" className="btn" onClick={this.handleClick}>+</button>
+                    <button value="4" className="btn" onClick={this.handleClick}>4</button>
+                    <button value="5" className="btn" onClick={this.handleClick}>5</button>
+                    <button value="6" className="btn" onClick={this.handleClick}>6</button>
+                    <button value="-" className="btn" onClick={this.handleClick}>-</button>
+                    <button value="7" className="btn" onClick={this.handleClick}>7</button>
+                    <button value="8" className="btn" onClick={this.handleClick}>8</button>
+                    <button value="9" className="btn" onClick={this.handleClick}>9</button>
+                    <button value="*" className="btn" onClick={this.handleClick}>*</button>
+                    <button value="0" className="btn" onClick={this.handleClick}>0</button>
+                    <button value="." className="btn" onClick={this.handleClick}>.</button>
+                    <button className="btn" onClick={this.clear}>AC</button>
+                    <button value="/" className="btn" onClick={this.handleClick}>/</button>
+                    <button id="back" onClick={this.back}>back</button>       
+                    <button id="AC" onClick={this.evaluate}>=</button>
+                </div>
+            </div>
         </div>
-      </div>
         )
     }
 
